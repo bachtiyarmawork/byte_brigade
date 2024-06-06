da71fbac72331d108ee7d685f0463b12b9407de7
Bachtiyar M. Arief <130717764+bachtiyarmawork@users.noreply.github.com>
2024-06-07 06:49:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,14 @@ st.set_page_config(
 )
 
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
+# Check if the required NLTK data is already downloaded
+if not all(os.path.exists(f"{nltk.data.find('stopwords')}.zip"),
+           os.path.exists(f"{nltk.data.find('punkt')}.zip"),
+           os.path.exists(f"{nltk.data.find('wordnet')}.zip")):
+    # Download the required NLTK data
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('wordnet')
 
 #@st.cache_data
 def extract_data():
